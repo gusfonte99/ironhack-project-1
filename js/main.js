d93b66fce01d99b118f7a417e548022f6f30cc53
Gustavo Fonte <gusfonte99@gmail.com>
2023-10-30 16:54:06 +0000

    add enemy class

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Player {
   constructor() {
-    this.positionX = 50; // maybe check how to do actual mid of screen
-    this.positionY = 50;
     this.width = 3;
     this.height = 5; // check notes
+    this.positionX = 15; /*  - (this.width / 2); */
+    this.positionY = 50; /*  - (this.height / 2); */
 
     this.playerElm = document.getElementById("player");
     this.playerElm.style.width = this.width + "vw";
@@ -31,9 +31,45 @@ class Player {
     this.positionX++;
     this.playerElm.style.left = this.positionX + "vw";
   }
+
+  getPosX(){
+    
+  } 
+  
+  getPosY(){
+    
+  }
+}
+
+class Enemy {
+  constructor() {
+    this.positionX = 70;
+    this.positionY = 50;
+    this.width = 3;
+    this.height = 5;
+
+    this.createDomElement();
+  }
+
+  createDomElement() {
+    this.enemyElm = document.createElement("div");
+    this.enemyElm.classList.add("enemy");
+
+    this.enemyElm.style.width = this.width + "vw";
+    this.enemyElm.style.height = this.height + "vh";
+    this.enemyElm.style.left = this.positionX + "vw";
+    this.enemyElm.style.bottom = this.positionY + "vh";
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.enemyElm);
+  }
+
+  // move towards player
+  followPlayer() {}
 }
 
 const player = new Player();
+const enemy = new Enemy();
 
 document.addEventListener("keydown", (e) => {
   switch (e.code) {
